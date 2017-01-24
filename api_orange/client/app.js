00f1ccec412d54e3d190c2437d2c0dbc3330d60a
valentin giraud <valentin.giraud@cprodirect.fr>
2017-01-24 20:18:55 +0100

    interface update2

--- api_orange/client/app.js
+++ api_orange/client/app.js
@@ -4,22 +4,18 @@ myApp.config(function($routeProvider){
 	$routeProvider.when('/clients', {
 		controller:'UsersController',
 		templateUrl: 'views/users.html'
-		
+
 	})
-	// .when('/clients', {
-	// 	controller:'UsersController',
-	// 	templateUrl: 'views/users.html'
-		
-	// })
 	.when('/clients/:_id', {
 		controller:'UsersController',
 		templateUrl: 'views/user.html'
-		
-		
 	})
 	.when('/login', {
 		controller:'LoginController',
 		templateUrl: 'views/login.html'
 	})
-	
+	.when('/delete', {
+		controller:'LoginController',
+		templateUrl: 'views/login.html'
+	})
 });
